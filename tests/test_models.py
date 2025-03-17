d066ca31d9df1fde5815e0ec3c81175e582159d6
Davies, Jenny <jenny.davies@mhra.gov.uk>
2025-03-17 14:50:42 +0000

    Corrected errors in tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -54,26 +54,22 @@ def test_daily_min_string():
         (
             [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
             [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+            None,
         ),
         (
             [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
             [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+            None,
         ),
         (
             [[float('nan'), 1, 1], [1, 1, 1], [1, 1, 1]],
             [[0, 1, 1], [1, 1, 1], [1, 1, 1]],
+            None,
         ),
         (
             [[1, 2, 3], [4, 5, float('nan')], [7, 8, 9]],
             [[0.33, 0.67, 1], [0.8, 1, 0], [0.78, 0.89, 1]],
-        ),
-        (
-            [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
-            [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
-        ),
-        (
-            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
-            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
         ),
         (
             [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
@@ -89,8 +85,8 @@ def test_daily_min_string():
 )
 
 def test_patient_normalise(test, expected, expect_raises):
-    """Test normalisation works for arrays of one and positive integers.
-    Test with a relative and absolute tolerance of 0.01"""
+    """Test normalisation works for arrays of one and positive integers."""
+        
     if expect_raises is not None:
         with pytest.raises(expect_raises):
             result = patient_normalise(np.array(test))
